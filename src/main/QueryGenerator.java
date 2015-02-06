e50eb0c91f6180e0c18334f3e4558d1c34d4c1c4
Adrian Sham <adrsham@cs.washington.edu>
2015-02-05 16:31:16 -0800

    added stub classes

---
+++ src/main/QueryGenerator.java
@@ -0,0 +1,13 @@
+package main;
+
+import Zql.ZQuery;
+
+public class QueryGenerator {
+	private QueryGenerator() {
+		//static class
+	}
+	
+	public static String generate(ZQuery originalQuery, Table original, Table modified) {
+		return null;
+	}
+}
---
+++ src/main/UpdateResultParser.java
@@ -0,0 +1,18 @@
+package main;
+
+public class UpdateResultParser {
+	private UpdateResultParser() {
+		//static util class
+	}
+	
+	/**
+	 * Parse string and turn it into table class
+	 * @param stringInput
+	 * @return
+	 */
+	public static Table parseStringToTable (String stringInput) {
+		return null;
+	}
+	
+	
+}
