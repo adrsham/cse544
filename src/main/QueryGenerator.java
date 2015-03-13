67db6c9b21160e1571d7a2b1d5a31102b90be822
Adrian Sham <adrsham@cs.washington.edu>
2015-03-12 18:44:45 -0700

    missed this file

--- src/main/QueryGenerator.java
+++ src/main/QueryGenerator.java
@@ -1,7 +1,11 @@
 package main;
 
+import java.math.BigInteger;
+import java.util.BitSet;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
 
@@ -26,9 +30,7 @@ public class QueryGenerator {
     }
     
     public static void getModifiedWhereExpressions(ZQuery originalQuery, ZQuery curModifiedQuery, Table modified) {
-        if (originalQuery.getWhere() == null ) {
-            return;
-        } else if (!(originalQuery.getWhere() instanceof ZExpression)) {
+        if (!(originalQuery.getWhere() instanceof ZExpression)) {
             System.err.println("Nested queries are currently unsupported.");
             System.err.println(originalQuery.getWhere());
             return;
@@ -38,7 +40,6 @@ public class QueryGenerator {
         // TODO currently going to assume that there is no alias going on. must fix this.
         curModifiedQuery.addWhere(whereClause);
         String res = con.runSQL(curModifiedQuery.toString());
-        //System.out.println(res);
         // table with same where as original sql but same projections as modified table from file.
         Table curOriginal = Util.parseStringToTable(res);
         if (curOriginal.equals(modified)) {
@@ -46,44 +47,112 @@ public class QueryGenerator {
             return;
         }
         Set<ZExpression> exp = getWhereClauses(whereClause);
-        if (curOriginal.contains(modified)) {
-            // the original is less restricting then the new table provided. that means new where clauses need to be added
-            // how do we do this?
             
-        } else if (modified.contains(curOriginal)) {
-            if (!recursiveRemoveWhere(new HashSet<ZExpression[]>(), exp, curModifiedQuery, modified)) {
-                System.err.println("System was not able to find matching SQL statement.");
+        //put each column of tuples into a set
+        HashMap<String, HashSet<Field>> fieldSet = modified.getSetOfFields();
+        //add all possible expressions for where into this set
+        //i.e name='adrian', id='1266067', major='comp sci'
+        for (Map.Entry<String, HashSet<Field>> entry : fieldSet.entrySet()) {
+            for (Field setEntry: entry.getValue()) {
+                if (setEntry instanceof IntField) {
+                    exp.add(new ZExpression("=", new ZConstant(entry.getKey(), ZConstant.COLUMNNAME), new ZConstant(setEntry.toString(), ZConstant.NUMBER)));
+                } else {
+                    exp.add(new ZExpression("=", new ZConstant(entry.getKey(), ZConstant.COLUMNNAME), new ZConstant(setEntry.toString(), ZConstant.STRING)));
+                }
             }
-        } else {
-            // still trying to figure this part out
         }
-        return;
+        ZExpression[] expArray = new ZExpression[exp.size()];
+        expArray = exp.toArray(expArray);
+        //enumerate all possible subsets starting from zero for where expressions
+        for (int i = 0; i < expArray.length; i++) {
+            for (BitSet bs : enumerateSubsetsBit(expArray.length, i)) {
+                //a bitset contains bits example : {1,0,1} 1 means true
+                //while expArray contains {a,b,c}
+                //matching the 2, currentSet would be {a,c}
+                Set <ZExpression> currentSet = new HashSet<>();
+                for (int j = 0; j < bs.length(); j++) {
+                    if (bs.get(j)) {
+                        currentSet.add(expArray[j]);
+                    }
+                }
+
+                curModifiedQuery.addWhere(convertSetToOneExp(currentSet));
+                String result = con.runSQL(curModifiedQuery.toString());
+                Table curMod = Util.parseStringToTable(result);
+                if (modified.equals(curMod)) {
+                    //if we get the same answer, we are done
+                    return;
+                }
+            }
+        }
     }
     
-    private static boolean recursiveRemoveWhere(Set<ZExpression[]> tried, Set<ZExpression> cur, ZQuery curModQuery, Table modified) {
-        if (tried.contains(cur)) {
-            return false;
-        } else {
-            ZExpression[] expArray = new ZExpression[cur.size()];
-            expArray = cur.toArray(expArray);
-            tried.add(expArray);
-            for (int i = 0; i < expArray.length; i++) {
-                cur.remove(expArray[i]);
-                // check to see if it is right or if it needs more removing.
-                curModQuery.addWhere(convertSetToOneExp(cur));
-                String res = con.runSQL(curModQuery.toString());
-                Table curMod = Util.parseStringToTable(res);
-                if (modified.equals(curMod)) {
-                    return true;
-                } else if (modified.contains(curMod) && recursiveRemoveWhere(tried, cur, curModQuery, modified)) {
-                    // current check is still contained in modified, need to get rid of more where clauses
-                    return true;
-                }
-                cur.add(expArray[i]);
-            }
-            return false;
-        }
+    /**
+     * Used to calculate the number of combination of r from n
+     * 
+     * @param n total number of n
+     * @param r size of r
+     * @return total number of combinations
+     */
+    private static long choose (int n, int r) {
+        return (factorial(n).divide(factorial(r).multiply(factorial(n-r)))).longValue();
     }
+    
+    /**
+     * Calculate the factorial of n
+     * 
+     * @param n number to calculate
+     * @return n!
+     */
+    private static BigInteger factorial (int n) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 1; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
+    }
+    
+    /**
+     * Enumerate all possible sets of "size" from v
+     * 
+     * @param fullSize full size of set to enumerate
+     * @param size size of subset
+     * @return BitSet representing the elements of v to choose
+     */
+    private static <T> Set<BitSet> enumerateSubsetsBit (int fullSize, int size) {
+        Set <BitSet> allSets = new HashSet<BitSet>((int)choose(fullSize, size));
+        BitSet start = new BitSet();
+        getBitCombinations(allSets, start, 0, 0, size, fullSize);
+        return allSets;
+    }
+    
+    /**
+     * Use recursion to calculate all possible subsets
+     * 
+     * @param allSets a set to store all bit set
+     * @param bs current bitset
+     * @param start start index of bitset to look at
+     * @param current current cardinality of bitset
+     * @param target target cardinality
+     * @param end end index of bitset
+     */
+    private static void getBitCombinations(Set<BitSet> allSets, BitSet bs, int start, int current, int target, int end) {
+        if (current == target) {
+            allSets.add((BitSet) bs.clone());
+            return;
+        }
+        if (start == end) {
+            return;
+        }
+        bs.set(start);
+        current++;
+        getBitCombinations(allSets, bs, start + 1, current, target, end);
+        
+        bs.clear(start);
+        current--;
+        getBitCombinations(allSets, bs, start +1, current, target, end);
+    }
+    
     
     private static ZExp convertSetToOneExp(Set<ZExpression> set) {
         if (set.size() == 0) {
@@ -102,6 +171,10 @@ public class QueryGenerator {
     @SuppressWarnings("unchecked")
     private static Set<ZExpression> getWhereClauses(ZExpression wc) {
         Set<ZExpression> res = new HashSet<ZExpression>();
+        if (wc == null) {
+            //if there is no where clause, return empty set
+            return res;
+        }
         if (wc.getOperator().equals("AND")) {
             for (int i = 0; i < wc.nbOperands(); i++) {
                 if (!(wc.getOperand(i) instanceof ZExpression)) {
